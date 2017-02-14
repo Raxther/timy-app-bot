813663f7d4fcb288ec8eb3303fed71fd82d1ba66
You Ask We Task <Timy@wificampus-020133.grenet.fr>
2017-02-14 17:19:04 +0100

    ok

--- app/controllers/conversations.js
+++ app/controllers/conversations.js
@@ -4,7 +4,28 @@
 module.exports = function (controller) {
   // this is triggered when a user clicks the send-to-messenger plugin
   controller.on('facebook_optin', function (bot, message) {
-    bot.reply(message, 'Welcome, friend')
+    var test = {
+    "attachment":{
+      "type":"template",
+      "payload":{
+        "template_type":"button",
+        "text":"What do you want to do next?",
+        "buttons":[
+          {
+            "type":"web_url",
+            "url":"https://petersapparel.parseapp.com",
+            "title":"Show Website"
+          },
+          {
+            "type":"postback",
+            "title":"Start Chatting",
+            "payload":"USER_DEFINED_PAYLOAD"
+          }
+        ]
+      }
+    }
+  };
+    bot.reply(message, test)
   })
 
   // user said hello
