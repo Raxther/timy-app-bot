c080f5be1c4cdcbd9f8ed096deb921f5a32b0069
You Ask We Task <Timy@wificampus-021002.grenet.fr>
2017-02-15 11:01:37 +0100

    ok

--- app/controllers/conversations.js
+++ app/controllers/conversations.js
@@ -9,29 +9,65 @@ module.exports = function (controller) {
 
   // user said hello
   controller.hears(['bonjour', 'salut', 'wesh','salu','coucou','yo'], 'message_received', function (bot, message) {
-    console.log("message");
-        var test = {
-    "attachment":{
-      "type":"template",
-      "payload":{
-        "template_type":"button",
-        "text":"Bonjour, tu es ici pour commander quelque chose ou tu veux des informations sur Timy ?",
-        "buttons":[
-          {
-            "type":"postback",
-            "title":"Je suis la pour passer commande",
-            "payload":"order"
-          },
-          {
-            "type":"web_url",
-            "url":"https://www.oculus.com/en-us/rift/",
-            "title":"Je veux plus d'information"
-          }
-        ]
-      }
-    }
-  }
-    bot.reply(message, test)
+    bot.startConversation(message, function(err, convo) {
+        convo.ask({
+            attachment: {
+                'type': 'template',
+                'payload': {
+                    'template_type': 'generic',
+                    'elements': [
+                        {
+                            'title': 'Classic White T-Shirt',
+                            'image_url': 'http://petersapparel.parseapp.com/img/item100-thumb.png',
+                            'subtitle': 'Soft white cotton t-shirt is back in style',
+                            'buttons': [
+                                {
+                                    'type': 'web_url',
+                                    'url': 'https://petersapparel.parseapp.com/view_item?item_id=100',
+                                    'title': 'View Item'
+                                },
+                                {
+                                    'type': 'web_url',
+                                    'url': 'https://petersapparel.parseapp.com/buy_item?item_id=100',
+                                    'title': 'Buy Item'
+                                },
+                                {
+                                    'type': 'postback',
+                                    'title': 'Bookmark Item',
+                                    'payload': 'White T-Shirt'
+                                }
+                            ]
+                        },
+                        {
+                            'title': 'Classic Grey T-Shirt',
+                            'image_url': 'http://petersapparel.parseapp.com/img/item101-thumb.png',
+                            'subtitle': 'Soft gray cotton t-shirt is back in style',
+                            'buttons': [
+                                {
+                                    'type': 'web_url',
+                                    'url': 'https://petersapparel.parseapp.com/view_item?item_id=101',
+                                    'title': 'View Item'
+                                },
+                                {
+                                    'type': 'web_url',
+                                    'url': 'https://petersapparel.parseapp.com/buy_item?item_id=101',
+                                    'title': 'Buy Item'
+                                },
+                                {
+                                    'type': 'postback',
+                                    'title': 'Bookmark Item',
+                                    'payload': 'Grey T-Shirt'
+                                }
+                            ]
+                        }
+                    ]
+                }
+            }
+        }, function(response, convo) {
+            // whoa, I got the postback payload as a response to my convo.ask!
+            convo.next();
+        });
+    });
   })
 
 // user click on button
