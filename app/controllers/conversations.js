b90ae276c8f8a8e4658e9bda62625b2a78bffaf0
You Ask We Task <Timy@wificampus-021002.grenet.fr>
2017-02-15 11:55:28 +0100

    ok

--- app/controllers/conversations.js
+++ app/controllers/conversations.js
@@ -65,7 +65,7 @@ module.exports = function (controller) {
             }
 
         }, function(response, convo) {
-          console.log("--------------------------------réponse " + response.type);
+          console.log("--------------------------------réponse " + response.text);
             convo.next();
         });
     });
